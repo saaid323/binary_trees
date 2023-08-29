86d4580f0b1f926837702d9b4419f6b2d459f596
saaid <saaid2538@gmail.com>
2023-08-29 10:12:29 -0700

    added 14-binary_tree_balance.c

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -9,13 +9,14 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	if (tree == NULL)
-		return (0);
-	size_t left_height = binary_tree_height(tree->left);
-	size_t right_height = binary_tree_height(tree->right);
-
-	return ((left_height > right_height) ? (left_height + 1) :
-			(right_height + 1));
+	if (tree != NULL)
+	{
+		size_t left_height = binary_tree_height(tree->left);
+		size_t right_height = binary_tree_height(tree->right);
+		return ((left_height > right_height) ? (left_height + 1) :
+				(right_height + 1));
+	}
+	return (0);
 }
 
 /**
@@ -25,10 +26,12 @@ size_t binary_tree_height(const binary_tree_t *tree)
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	if (tree == NULL)
-		return (0);
-	int left_height = binary_tree_height(tree->left);
-	int right_height = binary_tree_height(tree->right);
+	if (tree != NULL)
+	{
+		int left_height = binary_tree_height(tree->left);
+		int right_height = binary_tree_height(tree->right);
 
-	return (left_height - right_height);
+		return (left_height - right_height);
+	}
+	return (0);
 }

7d0177781c99e1c277e2471b5eb0db7ac50bb935
saaid <saaid2538@gmail.com>
2023-08-29 01:46:23 -0700

    added 2-binary_tree_insert_right.c

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -10,10 +10,6 @@
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-	if (parent == NULL)
-	{
-		return (NULL);
-	}
 	binary_tree_t *newNode = malloc(sizeof(binary_tree_t));
 
 	if (newNode == NULL)

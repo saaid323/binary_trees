37b4963a224627b433b53da611164018c7ad8116
saaid <saaid2538@gmail.com>
2023-08-29 01:42:46 -0700

    added 3-binary_tree_delete.c

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -6,7 +6,6 @@
  * binary_tree_insert_right - Inserts a node as the right-child of another node
  * @parent: Pointer to the node to insert the right-child in
  * @value: Value to store in the new node
- *
  * Return: Pointer to the created node, or NULL on failure or if parent is NULL
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)

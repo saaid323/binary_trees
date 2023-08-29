7440d0dca49edb67aeda7fcd1337e6f9d12fdcbf
saaid <saaid2538@gmail.com>
2023-08-29 04:18:27 -0700

    added 12-binary_tree_leaves.c

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -7,14 +7,16 @@
  */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-	if (tree != NULL)
+	if (tree == NULL)
 	{
-		if (tree->left == NULL && tree->right == NULL)
-			return (1);
-		size_t left_leaves = binary_tree_leaves(tree->left);
-		size_t right_leaves = binary_tree_leaves(tree->right);
-
-		return (left_leaves + right_leaves);
+		return (0);
 	}
-	return (0);
+	if (tree->left == NULL && tree->right == NULL)
+	{
+		return (1);
+	}
+	size_t left_leaves = binary_tree_leaves(tree->left);
+	size_t right_leaves = binary_tree_leaves(tree->right);
+
+	return (left_leaves + right_leaves);
 }

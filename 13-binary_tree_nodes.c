41b69a8016972e76ea0cca1031a7827ea24b9e3a
saaid <saaid2538@gmail.com>
2023-08-29 09:51:06 -0700

    added 13-binary_tree_nodes.c

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -7,14 +7,15 @@
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-	if (tree == NULL)
-		return (0);
-	size_t nodes = 0;
+	if (tree != NULL)
+	{
+		size_t nodes = 0;
 
-	if (tree->left || tree->right)
-		nodes++;
-	nodes += binary_tree_nodes(tree->left);
-	nodes += binary_tree_nodes(tree->right);
-	return nodes;
+		nodes += (tree->left || tree->right) ? 1 : 0;
+		nodes += binary_tree_nodes(tree->left);
+		nodes += binary_tree_nodes(tree->right);
+		return (nodes);
+	}
+	return (0);
 }
 

dd87a0ef1704486ecf736b45752d6981ccbb58f0
saaid <saaid2538@gmail.com>
2023-08-29 02:27:32 -0700

    added 4-binary_tree_is_leaf.c

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -8,6 +8,6 @@
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
 	if (node == NULL)
-		return 0;
-       	return (node->left == NULL && node->right == NULL);
+		return (0);
+	return (node->left == NULL && node->right == NULL);
 }

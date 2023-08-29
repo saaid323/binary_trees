88603848dbe2dfda2a79bb39e70be12254d90e76
saaid <saaid2538@gmail.com>
2023-08-29 01:50:14 -0700

    added 2-binary_tree_insert_right.c

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -16,7 +16,7 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	{
 		return (NULL);
 	}
-	if (parent == NULL)
+	else if (parent == NULL)
 		return (NULL);
 	newNode->n = value;
 	newNode->parent = parent;

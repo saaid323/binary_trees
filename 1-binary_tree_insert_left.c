709be4f7d560017a329409916a6a762b9babe79d
saaid <saaid2538@gmail.com>
2023-08-29 02:17:33 -0700

    added 1-binary_tree_insert_left.c

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -8,18 +8,21 @@
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *newNode = malloc(sizeof(binary_tree_t));
+	binary_tree_t *newNode;
+
+	if (parent == NULL)
+	{
+		return (NULL);
+	}
+	newNode = binary_tree_node(parent, value);
 
 	if (newNode == NULL)
 	{
 		return (NULL);
 	}
-	newNode->n = value;
-	newNode->parent = parent;
-	newNode->left = parent->left;
-	newNode->right = NULL;
 	if (parent->left != NULL)
 	{
+		newNode->left = parent->left;
 		parent->left->parent = newNode;
 	}
 	parent->left = newNode;
